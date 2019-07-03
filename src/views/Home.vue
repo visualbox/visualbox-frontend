c6f02d925f67c3080e870e3ffd5ced67850a3c62
Pwntus <pontus.edvard@gmail.com>
2019-07-04 01:56:58 +0200

    feat: change hero to video, improve explorer hover

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template lang="pug">
 v-container
   .aurora-borealis
+    video(
+      autoplay
+      loop
+      muted
+      :poster="require('../assets/img/hero.jpg')"
+    )
+      source(
+        :src="require('../assets/img/hero.mp4')"
+        type="video/mp4"
+      )
+      source(
+        :src="require('../assets/img/hero.webm')"
+        type="video/webm"
+      )
   v-responsive(:aspect-ratio="2.5/1")
     v-container(fill-height grid-list-xl)
       v-layout(align-center justify-center row wrap)
         v-flex(xs12 sm12 md8 lg8 xl6)
-          .display-3.font-weight-thin Data integration and
+          .display-3.font-weight-thin.mb-2 Data integration, AI and
           .display-3.font-weight-thin.mb-4 visualization toolbox
           .headline.font-weight-thin
             | Integrate any data source and create beautiful
@@ -39,8 +53,15 @@ export default Vue.extend({
     height 100%
     position absolute
     top 0; left 0; right 0;
-    background-image url(../assets/img/aurora-borealis.jpg)
-    background-size cover
+    overflow hidden
+    // background-image url(../assets/img/aurora-borealis.jpg)
+    // background-size cover
+
+    video
+      width 100%
+      height 100%
+      -o-object-fit cover
+      object-fit cover
 
   .v-responsive
     padding 64px 16px
@@ -49,8 +70,8 @@ export default Vue.extend({
     .v-card
       padding 40px 10px
       width 100%
-      text-align center
       color #FFF
+      text-align center
       background-color rgba(255, 255, 255, .25)
       border-radius 10px
 
